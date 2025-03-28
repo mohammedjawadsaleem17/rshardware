654c0afa5541ffd7615598b9f45190123f89827c
Mohammed Jawad Saleem <136219407+mohammedjawadsaleem17@users.noreply.github.com>
2025-03-28 15:13:33 +0530

    Added Prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from 'react';
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <div></div>;
 }
