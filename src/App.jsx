fe3ba3a1b1eaff57c06a6acca62c1152d80d3367
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2025-08-24 13:27:19 +0530

    Tweeking

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Navigation from "./components/Navigation";
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import Navigation from './components/Navigation';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    // Ping every 5 minutes (300000 ms)
+    const interval = setInterval(() => {
+      fetch('https://rshardware-backend.onrender.com/users')
+        .then(() => console.log('Backend pinged ✅'))
+        .catch((err) => console.error('Ping failed', err));
+    }, 300000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Navigation />
