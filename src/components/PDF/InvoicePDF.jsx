95bc62aa77a1e419d21bd1688f47921870a8d267
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2025-12-10 18:46:20 +0530

    Date Bug fix

--- src/components/PDF/InvoicePDF.jsx
+++ src/components/PDF/InvoicePDF.jsx
@@ -382,15 +382,11 @@ export default function InvoicePDF({ customerDetails, data = sampleData }) {
               </View>
               <View style={styles.infoRow}>
                 <Text style={styles.infoLabel}>Dated</Text>
-                <Text style={styles.infoValue}>
-                  {customerDetails?.invoiceDate}
-                </Text>
+                <Text style={styles.infoValue}>{customerDetails?.dated}</Text>
               </View>
               <View style={styles.infoRow}>
                 <Text style={styles.infoLabel}>Delivery Note Date</Text>
-                <Text style={styles.infoValue}>
-                  {customerDetails?.deliveryNoteDate}
-                </Text>
+                <Text style={styles.infoValue}>{customerDetails?.dated}</Text>
               </View>
               <View style={[styles.infoRowNoBorder]}>
                 <Text style={styles.infoLabel}>Destination</Text>
